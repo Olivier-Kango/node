6d8d7839d9776ab0732001f3e5a9a3fb9b5de131
Olivier-Kango <olivierkango@gmail.com>
2023-04-20 20:19:29 +0200

    Connect bdd to app

--- bdd.js
+++ bdd.js
@@ -1,1 +1,13 @@
 const { Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  user: 'postgres',
+  port: 5433,
+  password: 'Bleu111111',
+  database: 'postgres'
+});
+
+client.connect();
+const result = client.query('SELECT * FROM blagues;');
+console.log(result);
