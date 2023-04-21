737571c6fdf496c7d76c2b19369c124a49134488
Olivier-Kango <olivierkango@gmail.com>
2023-04-21 09:29:13 +0200

    Add id =1

--- bdd.js
+++ bdd.js
@@ -9,15 +9,14 @@ const client = new Client({
 });
 
 client.connect();
-result = client.query('SELECT * FROM blagues;')
+result = client.query('SELECT * FROM blagues WHERE id = 1;')
   .then((result) => {
     console.log(result.rows);
   })
 
   .catch((error) => {
-    console.log('Erreur survenue:', error.message);
-  })
-
+    console.error('Erreur survenue:' , error.message);
+  });
 // console.log(result);
 // console.log(1)
 // console.log(2)
