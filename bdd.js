02a76a66746e5511d6785de37d71adb7c4dfaf25
Olivier-Kango <olivierkango@gmail.com>
2023-04-22 10:56:11 +0200

    Paramètres de configuration

--- bdd.js
+++ bdd.js
@@ -1,3 +1,9 @@
 const { Client } = require('pg');
 
-const client = new Client();
+const client = new Client({
+  host: 'localhost',
+  user: 'postgres',
+  port: 5433,
+  password: 'Bleu111111',
+  database: 'postgres'
+});
