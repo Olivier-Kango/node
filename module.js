ac9ace251a0c22e73e45a69c8eb88de2c35f22dc
Olivier-Kango <olivierkango@gmail.com>
2023-04-27 18:59:52 +0200

    module

---
+++ module.js
@@ -0,0 +1,10 @@
+// const { cours, etudiants } = require('./scolarite');
+
+// console.log(cours, etudiants)
+// console.log(etudiants)
+
+// En Node.js il y a des modules qui sont directement integré, comme OS, FS, HTTP
+
+// const systeme = require('os');
+
+// console.log(systeme.userInfo(), systeme.version());
--- scolarite.js
+++ scolarite.js
@@ -1,1 +1,21 @@
 // Un module est une fonctionnalite organise dans un ou plusieurs fichier et qui peuvent être réutiliser
+
+const etudiants = [
+  {
+    nom: "Yvette Baruti",
+    niveau: 1
+  },
+  {
+    nom: "Jean Dupont",
+    niveau: 2
+  }
+];
+
+// console.log(etudiants);
+
+const cours = ["Geo", "Math", "Francais"];
+
+module.exports = {
+  etudiants,
+  cours
+};
