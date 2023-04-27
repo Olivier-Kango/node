31fb499b12a93d028699634279a776ba1ff4c2b7
Olivier-Kango <olivierkango@gmail.com>
2023-04-27 18:05:35 +0200

    Create ficher module

---
+++ global.js
@@ -0,0 +1,15 @@
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log("Salut le monde");
+//   clearInterval(temps);
+// }, 2000);
+
+// var temps = setInterval(() => {
+//   console.log("Execution...")
+// }, 500);
+
+// console.log(__filename);
+// console.log(__filename);
+
+
---
+++ scolarite.js
@@ -0,0 +1,1 @@
+// Un module est une fonctionnalite organise dans un ou plusieurs fichier et qui peuvent être réutiliser
