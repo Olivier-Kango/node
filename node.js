38dd608eb5e373e5c89a2137c23c8fb9961f5efd
Olivier-Kango <olivierkango@gmail.com>
2023-04-27 17:45:32 +0200

    Comment

---
+++ node.js
@@ -0,0 +1,20 @@
+// Environnement d'exécution de code JavaScript à la partie Serveur.
+
+// Pas besoin d'apprendre un nouveau
+// Très rapide et évolutif
+// Base de code très cohérent
+// Très grande communauté
+// Grand nombre de bibliothèque open Source.
+
+// JS client, pour rendre les pages web dynamique.
+
+// Comment ça marche?
+// Moteur V8 permet de compiler JS en code binaire.
+
+// PLAN DU COURS
+// Préliminaires de NodeJS
+// Gestion d'un serveur
+// Express GS
+// Manipulation de la base de données MYSQL
+
+// NOTIONS FONDAMENTALES NODE JS
