15fa246d2c2e33de2dbd8e30c7648a445f7fef98
Olivier-Kango <olivierkango@gmail.com>
2023-04-28 09:21:16 +0300

    Create file and folder

--- fichiers.js
+++ fichiers.js
@@ -1,19 +1,27 @@
 // Create and remove folder et file
 
 const { error } = require("console");
-const { mkdir, existsSync, rmdir } = require("fs");
+const { mkdir, existsSync, writeFile } = require("fs");
 
 // CREATE FOLDER
 
 if (existsSync('./mesFichiers')) {
   // console.log('le fichier existe déjà !!!')
-  rmdir('./mesFichiers', (error) => {
+  // rmdir('./mesFichiers', (error) => {
+  //   if(error) {
+  //     console.log(error.message)
+  //   } else {
+  //     console.log('Fichier effacé avec succès !')
+  //   }
+  // })
+  writeFile('./mesFichiers/file.txt', 'Bonjour Olivier, ceci est un message dessais',(error) => {
     if(error) {
-      console.log(error.message)
+      console.log(error.message);
     } else {
-      console.log('Fichier effacé avec succès !')
+      console.log('Le fichier a été créé avec succès !!!')
     }
-  })
+  })  
+
 } else {
   mkdir('./mesFichiers', (error) => {
     if(error) {
@@ -23,4 +31,3 @@ if (existsSync('./mesFichiers')) {
     }
   });
 }
-
