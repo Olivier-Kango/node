db33a20b414eb5483868cf236298bbd995f61c5d
Olivier-Kango <olivierkango@gmail.com>
2023-05-02 10:02:58 +0300

    Add send

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.get('/accueil', (req, res) => {
   res.status(200).send('Hello world !'), { root: __dirname }
-})
+});
+
+app.get('/profile', (req, res) => {
+  res.status(200).send('Hello world !'), { root: __dirname }
+});
 
 app.listen(3001, () => {
   console.log('en attente de la requête !')
