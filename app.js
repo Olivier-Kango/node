5ff46d447fa327f0e817f15616431eb135a2ef71
Olivier-Kango <olivierkango@gmail.com>
2023-05-02 10:12:21 +0300

    add root: __dirname

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/accueil', (req, res) => {
-  res.status(200).send('Hello world !'), { root: __dirname }
+  res.status(200).sendFile('IHM/accueil.html', { root: __dirname })
 });
 
-app.get('/profile', (req, res) => {
-  res.status(200).send('Hello world !'), { root: __dirname }
-});
 
 app.listen(3001, () => {
   console.log('en attente de la requête !')
