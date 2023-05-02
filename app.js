93053cdb1325980bae27c39a28a481cbd5522906
Olivier-Kango <olivierkango@gmail.com>
2023-05-02 09:52:11 +0300

    Console response

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
-console.log(express())
+const app = express();
+console.log(app);
+
+// app.get((req, res) => {
+//   res.status(200).send('Hello world !')
+// })
 
 // app.listen(3001, () => {
 //   console.log('en attente de la requête !')
