d95a382cdae2ae12e1fbd90f85d7e536dd6ac195
Olivier-Kango <olivierkango@gmail.com>
2023-05-02 10:13:55 +0300

    Add profile

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/accueil', (req, res) => {
   res.status(200).sendFile('IHM/accueil.html', { root: __dirname })
 });
 
+app.get('/profile', (req, res) => {
+  res.status(200).sendFile('IHM/profile.html', { root: __dirname })
+});
+
+app.get('/profile', (req, res) => {
+  res.status(200).sendFile('IHM/profile.html', { root: __dirname })
+});
+
 
 app.listen(3001, () => {
   console.log('en attente de la requête !')
